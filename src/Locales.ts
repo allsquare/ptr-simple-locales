f27b71c9aa77dd3a15b3efca0f209fe7c13d637d
Alexis Bertholom <alexis.bertholom.jd@gmail.com>
2019-09-13 16:44:58 +0200

    feat: remove date-formatting methods

--- src/Locales.ts
+++ src/Locales.ts
@@ -31,36 +31,6 @@ export default class Locales<LocaleT, ResourcesT extends LocaleResourcesBase>
       throw new Error(`Locales: invalid default locale: ${defaultLocale}`);
   }
 
-  public formatDate(date: Date): string
-  {
-    return date.toLocaleDateString(this.current.locale);
-  }
-
-  public formatTime(date: Date): string
-  {
-    return date.toLocaleTimeString(this.current.locale);
-  }
-
-  public formatDateTime(date: Date): string
-  {
-    return date.toLocaleString(this.current.locale);
-  }
-
-  public formatNullableDate(date: Date | null, nullString: string)
-  {
-    return date ? this.formatDate(date) : nullString;
-  }
-
-  public formatNullableTime(date: Date | null, nullString: string)
-  {
-    return date ? this.formatTime(date) : nullString;
-  }
-
-  public formatNullableDateTime(date: Date | null, nullString: string)
-  {
-    return date ? this.formatDateTime(date) : nullString;
-  }
-
   public get locales()
   {
     return this._mappings;
